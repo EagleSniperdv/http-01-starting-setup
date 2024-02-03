082953b58b59435bdca4642ff2cf21eb0812ede1
EagleSniperdv <106923143+EagleSniperdv@users.noreply.github.com>
2024-02-03 13:49:25 +0300

    http req and error handling

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,9 +3,14 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button
+          @click="loadExperience"
+        >Load Submitted Experiences</base-button>
       </div>
-      <ul>
+      <p v-if="isLoading">Loading Results...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && (!results || results.length === 0)">No stored surveys</p>
+      <ul >
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,10 +26,54 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  
+  data() {
+    return {
+      results: [ ],
+      isLoading: true,
+      error: null
+    }
+  },
   components: {
     SurveyResult,
   },
+
+  methods: {
+    loadExperience() {
+      this.isLoading = true;
+      this.error = null;
+      // fetch('https://vue3-app-359e7-default-rtdb.firebaseio.com/surveys.json')
+      fetch('https://vue3-app-359e7-default-rtdb.firebaseio.com/surveys.json')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      }).then((data) => {
+        this.isLoading = false;
+        const results = [];
+
+        for (const id in data) {
+          results.push({
+            id: id,
+            name: data[id].name,
+            rating: data[id].rating
+          });
+        }
+
+        this.results = results;
+        // console.log(data)
+      }).catch((error) => {
+        this.isLoading = false;
+        console.log(error);
+        this.error = 'Loading Error please try again later...'
+
+      });
+    }
+  },
+
+  mounted() {
+    this.loadExperience();
+  }
 };
 </script>
 
